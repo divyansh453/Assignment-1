05371a3f250849e2d1803f32f1c1d9c27469b94b
divyansh453 <divyanshurana312@gmail.com>
2023-05-14 12:02:29 +0530

    remove some restrictions

--- apis/models.py
+++ apis/models.py
@@ -2,19 +2,6 @@ from django.db import models
 from django.utils import timezone
 
 class Blogs(models.Model):
-    tech_stacks=[
-        ('FRONTEND','FRONTEND'),
-        ('BACKEND','BACKEND'),
-        ('APP DEVELOPMENT','APP DEVELOPMENT'),
-        ('MACHINE LEARNING','MACHINE LEARNING'),
-        ('DJANGO','DJANGO'),
-        ('REACTJS','REACTJS'),
-        ('NODEJS','NODEJS'),
-        ('FLUTTER','FLUTTER'),
-        ('KOTLIN','KOTLIN'),
-        ('PYTHON','PYTHON'),
-        ('OTHERS','OTHERS')
-    ]
     body=models.TextField(null=False)
-    tech=models.CharField(choices=tech_stacks,null=True,blank=True,max_length=18)
+    tech=models.CharField(null=True,blank=True,max_length=18)
     createdAt=models.DateTimeField(default=timezone.now)
